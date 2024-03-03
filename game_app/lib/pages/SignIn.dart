c8776c13df2c0421b884e3f920d0b9401d8e19b4
Yisroel Rosenblat <srulsterer@gmail.com>
2024-03-03 10:52:32 -0500

    added anonymous login

--- game_app/lib/pages/SignIn.dart
+++ game_app/lib/pages/SignIn.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 
@@ -17,6 +19,20 @@ class SignIn extends StatelessWidget {
               '165156080497-t4lqeabpg69q6ld1tl6l92jk0v3e88mg.apps.googleusercontent.com',
         ),
       ],
+      footerBuilder: (context, acion) {
+        return FilledButton(
+          onPressed: () {
+            FirebaseAuth.instance.signInAnonymously();
+          },
+          child: const Text("Sign In Anonymously"),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.grey),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+          ),
+        );
+      },
     );
   }
 }
